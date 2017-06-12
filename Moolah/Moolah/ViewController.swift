6e5beba679e0e5283a86c2a5a2313b70c40ac9a9
Arvin San Miguel <arb1nsnmgl@gmail.com>
2017-06-11 21:50:06 -0400

    Add files via upload

---
+++ Moolah/Moolah/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moolah
+//
+//  Created by Arvin San Miguel on 6/11/17.
+//  Copyright © 2017 Appr1sing Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
